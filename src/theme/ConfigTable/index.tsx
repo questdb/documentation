7b7bddb3130f40baa67800022c2ec124b1bd9258
Andrei Pechkurov <37772591+puzpuzpuz@users.noreply.github.com>
2025-01-07 21:27:25 +0200

    Document reload_config SQL function (#117)
    Follow-up for https://github.com/questdb/questdb/pull/5183
    
    ---------
    
    Co-authored-by: goodroot <9484709+goodroot@users.noreply.github.com>

--- src/theme/ConfigTable/index.tsx
+++ src/theme/ConfigTable/index.tsx
@@ -1,7 +1,13 @@
 import ReactMarkdown from "react-markdown"
 
 type Props = {
-  rows: { [key: string]: { default: string; description: string } }
+  rows: {
+    [key: string]: {
+      default: string
+      description: string
+      reloadable?: boolean
+    }
+  }
   pick?: string[]
 }
 
@@ -12,16 +18,18 @@ export const ConfigTable = ({ rows, pick }: Props) => {
         <tr>
           <th>Property</th>
           <th>Default</th>
+          <th>Reloadable</th>
           <th>Description</th>
         </tr>
       </thead>
       <tbody>
         {Object.entries(rows)
           .filter(([key]) => (Array.isArray(pick) ? pick.includes(key) : true))
-          .map(([key, { default: defaultValue, description }]) => (
+          .map(([key, { default: defaultValue, description, reloadable }]) => (
             <tr key={key}>
               <td className="property-cell">{key}</td>
               <td>{defaultValue}</td>
+              <td>{reloadable ? "Yes" : "No"}</td>
               <td>
                 <ReactMarkdown>{description}</ReactMarkdown>
               </td>
