29adba2bc8d6b7a93c9914896ab28aaf0d4a4e1c
Jaromir Hamala <jaromir.hamala@gmail.com>
2025-05-09 18:57:20 +0200

    r logo

--- src/components/Clients/index.tsx
+++ src/components/Clients/index.tsx
@@ -126,7 +126,7 @@ const clientsData: Client[] = [
     name: 'R',
     description:
       'R is a programming language and free software environment for statistical computing and graphics supported by the R Foundation for Statistical Computing.',
-    logo: '/images/logos/php.svg',
+    logo: '/images/logos/r.svg',
     protocol: 'PGWire',
   },
 ];
